10c4c0737cd3aabd37576c3ba94ec017e96f5d57
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-07-25 15:31:13 +0600

    done

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ function reverseString(inputString) {
 const inputStr = "hello world";
 const outputStr = reverseString(inputStr);
 console.log(outputStr); // Output: "dlrow olleh"
+
+
+function sumOfPositiveNumbers(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      sum += arr[i];
+    }
+  }
+  return sum; 
+}
+
+// Test the function
+const inputArr = [2, -5, 10, -3, 7];
+const outputSum = sumOfPositiveNumbers(inputArr);
+console.log(outputSum); // Output: 19
