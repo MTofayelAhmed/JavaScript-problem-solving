27517c6769304d5df628dda54a6b466421f790f0
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-07-27 13:35:25 +0600

    done with javascript problem solving

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Task1
+
 function reverseString(inputString) {
   let reversedString = "";
   for (let i = inputString.length - 1; i >= 0; i--) {
@@ -11,7 +13,7 @@ const inputStr = "hello world";
 const outputStr = reverseString(inputStr);
 console.log(outputStr); // Output: "dlrow olleh"
 
-
+// Task2
 function sumOfPositiveNumbers(arr) {
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
@@ -30,64 +32,63 @@ console.log(outputSum); // Output: 19
 
 
 
-
-// function findMostFrequentElement(arr) {
-//     let frequencyCounter = {};
-//     let maxFrequency = 0;
-//     let mostFrequentElement;
+// Task 3
+function findMostFrequentElement(arr) {
+    let frequencyCounter = {};
+    let maxFrequency = 0;
+    let mostFrequentElement;
   
-//     for (const element of arr) {
-//       if (frequencyCounter[element]) {
-//         frequencyCounter[element]++;
-//       } else {
-//         frequencyCounter[element] = 1;
-//       }
+    for (const element of arr) {
+      if (frequencyCounter[element]) {
+        frequencyCounter[element]++;
+      } else {
+        frequencyCounter[element] = 1;
+      }
   
-//       if (frequencyCounter[element] > maxFrequency) {
-//         maxFrequency = frequencyCounter[element];
-//         mostFrequentElement = element;
-//       }
-//     }
+      if (frequencyCounter[element] > maxFrequency) {
+        maxFrequency = frequencyCounter[element];
+        mostFrequentElement = element;
+      }
+    }
   
-//     return mostFrequentElement;
-//   }
+    return mostFrequentElement;
+  }
   
-//   // Test the function
-//   const input = [3, 5, 2, 5, 3, 3, 1, 4, 5];
-//   const mostFrequent = findMostFrequentElement(input);
-//   console.log(mostFrequent); // Output: 3
+  // Test the function
+  const input = [3, 5, 2, 5, 3, 3, 1, 4, 5];
+  const mostFrequent = findMostFrequentElement(input);
+  console.log(mostFrequent);
   
-
-// function findTwoNumbersWithSum(arr, target) {
-//     let left = 0;
-//     let right = arr.length - 1;
+// Task Four
+function findTwoNumbersWithSum(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
   
-//     while (left < right) {
-//       const sum = arr[left] + arr[right];
+    while (left < right) {
+      const sum = arr[left] + arr[right];
   
-//       if (sum === target) {
-//         // If the sum matches the target, return the indices of the two numbers
-//         return [left, right];
-//       } else if (sum < target) {
-//         // If the sum is less than the target, move the left pointer to consider a larger number
-//         left++;
-//       } else {
-//         // If the sum is greater than the target, move the right pointer to consider a smaller number
-//         right--;
-//       }
-//     }
+      if (sum === target) {
+        
+        return [left, right];
+      } else if (sum < target) {
+        
+        left++;
+      } else {
+     
+        right--;
+      }
+    }
+    return [];
+  }
   
-//     // If the loop finishes without finding a pair, return an empty array
-//     return [];
-//   }
-  
-//   // Test the function
-//   const sortedArray = [1, 3, 9, 6, 11, 15];
-//   const targetValue = 9;
-//   const result = findTwoNumbersWithSum(sortedArray, targetValue);
-//   console.log(result); // Output: [1, 2] (numbers at indices 0 and 3: 1 + 8 = 9)
+  // Test the function
+  const sortedArray = [1, 3, 9, 6, 11, 15];
+  const targetValue = 9;
+  const result1 = findTwoNumbersWithSum(sortedArray, targetValue);
+  console.log(result1); // Output: [1, 2] (numbers at indices 0 and 3: 1 + 8 = 9)
   
 
+// Task5
 function calculate(num1, num2, operator) {
   switch (operator) {
     case '+':
@@ -106,8 +107,59 @@ function calculate(num1, num2, operator) {
   }
 }
 
-
-
 const result = calculate(10, 20, '')
 console.log(`Result: ${result}`);
 
+
+// Task6
+function generateRandomPassword(length) {
+  const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+  const numbers = '0123456789';
+  const specialCharacters = '!@#$%^&*()-_=+[]{}|;:,.<>?';
+
+  const allCharacters = uppercaseLetters + lowercaseLetters + numbers + specialCharacters;
+  let password = '';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * allCharacters.length);
+    password += allCharacters.charAt(randomIndex);
+  }
+
+  return password;
+}
+
+
+const passwordLength = 12; 
+const randomPassword = generateRandomPassword(passwordLength);
+console.log(randomPassword);
+
+// Task8
+function findSecondSmallest(arr) {
+  if (arr.length < 2) {
+    throw new Error("Array must contain at least two numbers.");
+  }
+
+  let smallest = Infinity;
+  let secondSmallest = Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < smallest) {
+      secondSmallest = smallest;
+      smallest = arr[i];
+    } else if (arr[i] < secondSmallest && arr[i] !== smallest) {
+      secondSmallest = arr[i];
+    }
+  }
+
+  if (secondSmallest === Infinity) {
+    throw new Error("No second smallest element found.");
+  }
+
+  return secondSmallest;
+}
+
+
+const numbers = [5, 2, 8, 1, 3];
+const secondSmallest = findSecondSmallest(numbers);
+console.log("Second Smallest:", secondSmallest);
